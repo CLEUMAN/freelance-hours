5eb635c23f7740d8e88372e554e7de5131edfa2c
CLEUMAN <cleuman11@hotmail.com>
2024-10-08 21:36:34 -0300

    Criação dos models User e Project

---
+++ app/Models/Project.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Project extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProjectFactory> */
+    use HasFactory;
+
+    public function casts(){
+        return [
+            'tech_stack' => 'array',
+        ];
+    }
+}
